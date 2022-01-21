8f1aa75e207743954325a45415ecacf4e91d3de6
Daniel R. Little <82964069+dlittle9@users.noreply.github.com>
2022-01-21 15:34:27 -0500

    Adding Apply Run All

---
+++ terragrunt-action/src/terragrunt_apply_run_all.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+function terragruntApplyRunAll {
+    # Gather the output of `terragrunt apply`.
+    echo "apply: info: applying Terragrunt configuration in ${tfWorkingDir}"
+    applyOutput=$(${tfBinary} apply -auto-approve -input=false ${*} 2>&1)
+    applyExitCode=${?}
+    applyCommentStatus="Failed"
+    
+    # Exit code of 0 indicates success. Print the output and exit.
+    if [ ${applyExitCode} -eq 0 ]; then
+        echo "apply: info: successfully applied Terragrunt configuration in ${tfWorkingDir}"
+        echo "${applyOutput}"
+        echo
+        applyCommentStatus="Success"
+    fi
+    
+    # Exit code of !0 indicates failure.
+    if [ ${applyExitCode} -ne 0 ]; then
+        echo "apply: error: failed to apply Terragrunt configuration in ${tfWorkingDir}"
+        echo "${applyOutput}"
+        echo
+    fi
+    
+    exit ${applyExitCode}
+}
